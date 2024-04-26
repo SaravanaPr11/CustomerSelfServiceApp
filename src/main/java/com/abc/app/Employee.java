8cc4e8bb6306eae3e44ecb5cef3d97190c4e02cb
Rohankrish <168076678+Rohankrish@users.noreply.github.com>
2024-04-26 06:57:18 +0530

    create a employee

---
+++ src/main/java/com/abc/app/Employee.java
@@ -0,0 +1,51 @@
+package com.abc.app;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+	
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  @Column(name = "employee_id")
+		 private int employee_id;
+	  
+	  @Column(name = "userName", length = 25, nullable = false)
+		 private String userName;
+	
+	  @Column(name = "password", length = 15, nullable = false)
+		 private String password;
+
+	public int getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	  
+	  
+
+}
