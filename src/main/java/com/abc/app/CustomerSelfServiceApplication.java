413b6a75a1b91b12565e306b635f877479b12dc7
Rohankrish <168076678+Rohankrish@users.noreply.github.com>
2024-04-29 17:04:59 +0530

    update5

--- src/main/java/com/abc/app/CustomerSelfServiceApplication.java
+++ src/main/java/com/abc/app/CustomerSelfServiceApplication.java
@@ -3,10 +3,14 @@ package com.abc.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EntityScan("com.abc.model")
 @SpringBootApplication
+@EnableJpaRepositories("com.abc.repository")
+@ComponentScan("com.abc")
 public class CustomerSelfServiceApplication {
 
 	public static void main(String[] args) {
